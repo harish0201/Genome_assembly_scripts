890c2dd83a44bff5e87e1069bba43781e4316820
Harish <harish0201@users.noreply.github.com>
2020-04-25 21:41:09 +0530

    Create proteins_cluster.sh
    Uses mmseqs2 to cluster the proteins sequences. I use this for generating a Non-Redundant protein set to generate alignments using genomethreader or whatever.
    
    Has been generalized enough.

---
+++ proteins_cluster.sh
@@ -0,0 +1,14 @@
+#Requires mmseqs2; get it here: https://github.com/soedinglab/MMseqs2
+#Requires concatenated protein set from multiple species
+#Requires seqtk;
+#Requires seqkit;
+
+export PATH=$PATH:"$1"
+seqtk seq -m 10 "$2" > "$2".tmp.fa
+mmseqs createdb "$2".tmp.fa proteins_mmseqs
+mmseqs cluster --cov-mode 1 -c 0.5 --min-seq-id 0.5 proteins_mmseqs cluster $PWD
+mmseqs result2flat proteins_mmseqs proteins_mmseqs cluster cluster_out --use-fasta-header
+grep "^>" cluster_out | sed 's/>//g' | seqtk subseq "$2".tmp.fa - > uniq_proteins.fasta
+mkdir proteins && mv "$2" uniq_proteins.fasta proteins
+rm -rf proteins_mmseqs* latest cluster* [0-9][0-9][0-9]*
+echo "done"
