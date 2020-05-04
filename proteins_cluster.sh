bb07f82dfeac59bd18b4fc15b248afb3f7087144
Harish <harish0201@users.noreply.github.com>
2020-05-04 19:43:11 +0530

    Update proteins_cluster.sh
    add vars to enable selection of seq-id and seq-coverage

--- proteins_cluster.sh
+++ proteins_cluster.sh
@@ -2,11 +2,11 @@
 #Requires concatenated protein set from multiple species
 #Requires seqtk;
 #Requires seqkit;
-
+#$3 and $4 are the query coverage and seq-id respectively
 export PATH=$PATH:"$1"
 seqtk seq -m 10 "$2" > "$2".tmp.fa
 mmseqs createdb "$2".tmp.fa proteins_mmseqs
-mmseqs cluster --cov-mode 1 -c 0.5 --min-seq-id 0.5 proteins_mmseqs cluster $PWD
+mmseqs cluster --cov-mode 1 -c "$3" --min-seq-id "$4" proteins_mmseqs cluster $PWD
 mmseqs result2flat proteins_mmseqs proteins_mmseqs cluster cluster_out --use-fasta-header
 grep "^>" cluster_out | sed 's/>//g' | seqtk subseq "$2".tmp.fa - > uniq_proteins.fasta
 mkdir proteins && mv "$2" uniq_proteins.fasta proteins
