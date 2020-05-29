47ac7c6094df471904fde7374c3dafddcb778f88
Harish <harish0201@users.noreply.github.com>
2020-05-29 20:27:16 +0530

    Update proteins_align_genome.sh
    Add filtering steps

--- proteins_align_genome.sh
+++ proteins_align_genome.sh
@@ -9,6 +9,9 @@ query=$(basename "$1" .fasta)
 tmp=tmp_$(basename "$1" .fasta)
 mmseqs createdb $1 $query
 mmseqs search $query "$2" out_$query $tmp -e 0.0001 --threads 96 -s 8.5 -a --max-seqs 500 --orf-start-mode 1 --comp-bias-corr 0 --mask 0
-output=$query.out.gemoma
+output=$query.fmt
 mmseqs convertalis $query target out_$query $output --format-output "query,target,pident,alnlen,mismatch,gapopen,qstart,qend,tstart,tend,evalue,bits,empty,raw,nident,empty,empty,empty,qframe,tframe,qaln,taln,qlen,tlen"
+awk -F'\t' '{if($3>=0.8 && $11<=0.0001 && $12>=50)print $0}' $output > $output.0.8id_0.0001eval_50bits.txt
+awk -F'\t' '!arr[$2,$1]++' <(awk '{print $2"\t"$1}' $output.0.8id_0.0001eval_50bits.txt) | cut -f1-2 | sort -Vk1 > sorted.hits
+echo "sorted.hits can be used with genomethreader to perform a filtered alignment"
 echo "done"
