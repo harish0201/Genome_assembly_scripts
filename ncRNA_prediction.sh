80c52457a1cc83957452be79a5f2a37b5ad9b8e4
Harish <harish0201@users.noreply.github.com>
2022-10-30 23:16:34 -0400

    Update ncRNA_prediction.sh
    index -> faidx

--- ncRNA_prediction.sh
+++ ncRNA_prediction.sh
@@ -5,7 +5,7 @@ mkdir split_genome && cd split_genome
 ln -s "$1" genome.fasta
 python2.7 split-assembly.py genome.fasta > /dev/null
 unlink genome.fasta
-for i in *.fa; do echo "samtools index "$i""; done | parallel -j100
+for i in *.fa; do echo "samtools faidx "$i""; done | parallel -j100
 for i in *.fa; do echo "cmscan -Z $(awk '{$x=$2*2/10^9; print $x}' "$i".fai) --cut_ga --rfam --nohmmonly --tblout $(basename "$i" .fa).tblout --fmt 2 --clanin Rfam.clanin Rfam.cm "$i" >
 $(basename "$i" .fa).cmscan"; done | parallel -j30
 cat *.cmscan > ../result.cmscan
